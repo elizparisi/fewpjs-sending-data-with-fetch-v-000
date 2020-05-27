b413a186536dfbd25fa2ac8bc7be335280fd5058
Liz P <parisielizabeth@gmail.com>
2020-05-27 02:49:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,18 @@
 
 function submitData(name, email) {
   
-}
+  let formData = {
+  dogName: "Byron",
+  dogBreed: "Poodle"
+};
+ 
+let configObj = {
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(formData)
+};
+ 
+fetch("http://localhost:3000/dogs", configObj)
+};
